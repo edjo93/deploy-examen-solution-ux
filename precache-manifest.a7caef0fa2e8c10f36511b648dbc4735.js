92288967cd52a067edb9a944afbcd0fd79c11d72
edjo93 <eddas.carrasco@unitec.edu>
2020-08-21 17:14:02 -0600

    Updates

---
+++ precache-manifest.a7caef0fa2e8c10f36511b648dbc4735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205fec443990335c58df767263996a7f",
+    "url": "/deploy-examen-solution-ux/index.html"
+  },
+  {
+    "revision": "1887ca1a1a47fa13d083",
+    "url": "/deploy-examen-solution-ux/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3b5e71996f72eebd1e9",
+    "url": "/deploy-examen-solution-ux/static/js/2.af1e3377.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-examen-solution-ux/static/js/2.af1e3377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1887ca1a1a47fa13d083",
+    "url": "/deploy-examen-solution-ux/static/js/main.d5293cd4.chunk.js"
+  },
+  {
+    "revision": "679583566400d3631b6e",
+    "url": "/deploy-examen-solution-ux/static/js/runtime-main.4ecac6c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-examen-solution-ux/static/media/logo.5d5d9eef.svg"
+  }
+]);
